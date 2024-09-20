5a784347610a6a197c41f38e6615fc9fe2b0781e
skorzany <177423232+skorzany@users.noreply.github.com>
2024-09-20 17:39:21 +0200

    Add a function for computer's choice

--- rock-paper-scissors.js
+++ rock-paper-scissors.js
@@ -1,1 +1,10 @@
-console.log("Hello World!");
+function getComputerChoice() {
+    const choices = ["rock", "paper", "scissors"];
+    const choice = Math.floor(Math.random()*choices.length);
+
+    return choices[choice];
+}
+
+for (let i = 0; i < 3; i++){
+    console.log(getComputerChoice());
+}
