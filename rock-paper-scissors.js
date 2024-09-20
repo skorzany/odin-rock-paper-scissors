e7ff3d2cacfabd51edecffd50657a1282fb47493
skorzany <177423232+skorzany@users.noreply.github.com>
2024-09-20 18:14:30 +0200

    Modify the getComputerChoice for performance

--- rock-paper-scissors.js
+++ rock-paper-scissors.js
@@ -1,10 +1,15 @@
 function getComputerChoice() {
-    const choices = ["rock", "paper", "scissors"];
-    const choice = Math.floor(Math.random()*choices.length);
+    const choices = new Map([
+        [0, "rock"],
+        [1, "paper"],
+        [2, "scissors"],
+    ]);
+    const choice = Math.floor(Math.random()*choices.size);
 
-    return choices[choice];
+    return choices.get(choice);
 }
 
+// test for getComputerChoice
 for (let i = 0; i < 3; i++){
     console.log(getComputerChoice());
 }
