dbe7681e1f3408b01110797a5ae16424c3669ed0
skorzany <177423232+skorzany@users.noreply.github.com>
2024-09-24 17:51:15 +0200

    Add functions for playing a single round

--- rock-paper-scissors.js
+++ rock-paper-scissors.js
@@ -9,10 +9,10 @@ function getComputerChoice() {
     return choices.get(choice);
 }
 
-// test for getComputerChoice
-for (let i = 0; i < 3; i++){
-    console.log(getComputerChoice());
-}
+// // test for getComputerChoice
+// for (let i = 0; i < 3; i++){
+//     console.log(getComputerChoice());
+// }
 
 
 function getHumanChoice() {
@@ -22,7 +22,76 @@ function getHumanChoice() {
     return choices.has(choice) ? choice : null;
 }
 
-// test for getHumanChoice
-for (let i = 0; i < 3; i++){
-    console.log(getHumanChoice());
+// // test for getHumanChoice
+// for (let i = 0; i < 3; i++){
+//     console.log(getHumanChoice());
+// }
+
+
+function calculateResult(humanChoice, computerChoice, ruleset) {
+    return ruleset.get(humanChoice).indexOf(computerChoice[0]) - 1;
 }
+
+
+// //tests for calculateResult
+// const rules = new Map([
+//     ["rock", "prs"],
+//     ["paper", "spr"],
+//     ["scissors", "rsp"],
+// ]);
+
+// console.assert(calculateResult("rock", "rock", rules) === 0, "ROCK tie error");
+// console.assert(calculateResult("paper", "paper", rules) === 0, "PAPER tie error");
+// console.assert(calculateResult("scissors", "scissors", rules) === 0, "SCISSORS tie error");
+// console.assert(calculateResult("rock", "paper", rules) === -1, "ROCK lose error");
+// console.assert(calculateResult("paper", "scissors", rules) === -1, "PAPER lose error");
+// console.assert(calculateResult("scissors", "rock", rules) === -1, "SCISSORS lose error");
+// console.assert(calculateResult("rock", "scissors", rules) === 1, "ROCK win error");
+// console.assert(calculateResult("paper", "rock", rules) === 1, "PAPER win error");
+// console.assert(calculateResult("scissors", "paper", rules) === 1, "SCISSORS win error");
+
+
+function playRound(humanChoice, computerChoice) {
+    while (humanChoice === null) {
+        alert("Your choice was invalid! Try again.");
+        humanChoice = getHumanChoice();
+    }
+
+    const rules = new Map([
+        ["rock", "prs"],
+        ["paper", "spr"],
+        ["scissors", "rsp"],
+    ]);
+
+    const result = calculateResult(humanChoice, computerChoice, rules);
+    switch (result) {
+        case -1:
+            msg = `You lose! ${
+                computerChoice[0].toUpperCase() + computerChoice.substr(1)
+            } beats ${
+                humanChoice[0].toUpperCase() + humanChoice.substr(1)
+            }`;
+            break;
+        case 0:
+            msg = `It's a tie! You both chose ${
+                humanChoice[0].toUpperCase() + humanChoice.substr(1)
+            }`;
+            break;
+        case 1:
+            msg = `You win! ${
+                humanChoice[0].toUpperCase() + humanChoice.substr(1)
+            } beats ${
+                computerChoice[0].toUpperCase() + computerChoice.substr(1)
+            }`;
+            break;
+    }
+
+    return msg;
+}
+
+// tests for playRound
+console.assert(playRound("paper", "paper") === "It's a tie! You both chose Paper", "paper tie error");
+console.assert(playRound("rock", "rock") === "It's a tie! You both chose Rock", "rock tie error");
+console.assert(playRound("scissors", "scissors") === "It's a tie! You both chose Scissors", "scissors tie error");
+
+console.log(playRound(getHumanChoice(), getComputerChoice()));
